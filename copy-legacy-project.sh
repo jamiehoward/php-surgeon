b34416328edc33048b283f1755e53169d334240b
Jamie Howard <jamiehowardco@gmail.com>
2019-02-12 23:44:35 -0500

    Completed working version of script. Removed cache from repo

--- copy-legacy-project.sh
+++ copy-legacy-project.sh
@@ -2,6 +2,8 @@
 
 SOURCE_FILE=$1
 DESTINATION_PROJECT=$(pwd)/$2
+HISTORY_FILE=./cache/history.list
+SEARCH_FILE=./cache/search.list
 
 # Initialize the project
 if [ ! -e $DESTINATION_PROJECT ]; then
@@ -12,14 +14,28 @@ if [ ! -e './cache/' ]; then
 	mkdir cache
 fi
 
+rm $HISTORY_FILE && touch $HISTORY_FILE
+rm $SEARCH_FILE && touch $SEARCH_FILE
+
 function copyAllDependencies() {
 	RELDIR=`dirname $SOURCE_FILE`
 
 	while read FILE; do
-		FILEDIR=`dirname $RELDIR/$FILE | sed -e 's/.*\.\.\///g'`
-		mkdir -p $DESTINATION_PROJECT/$FILEDIR
-		cp $RELDIR/$FILE $DESTINATION_PROJECT/$FILEDIR/
-	done < $CACHE_FILE
+		# Make the necessary file structure
+		FILE_DIR=`dirname $FILE`
+
+		mkdir -p $DESTINATION_PROJECT$FILE_DIR
+		cp $FILE $DESTINATION_PROJECT$FILE_DIR/
+	done < $HISTORY_FILE
+}
+
+function sitesRemainToBeSearched() {
+	CONTENTS=$(cat $SEARCH_FILE | wc -c)
+	if [[ $CONTENTS -gt 2 ]]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 function realPath() {
@@ -28,24 +44,51 @@ function realPath() {
 	echo $REALPATH/$(basename $1)
 }
 
-function findDependencies() {
-	FOUND=`grep 'include(\|include_once(\|require(\|require_once(' $1 | grep -v '/\*' | grep -v 'require_once($\|require($\|include_once($\|include($' | sed -e "s/.*require('//g;s/.*require_once('//g;s/.*require(\"//g;s/.*require_once(\"//g;s/.*include('//g;s/.*include_once('//g;s/.*include(\"//g;s/.*include_once(\"//g;s/').*//g;s/\").*//g" | sort -u`
-
-	for FILE in $FOUND; do
-		RELDIR=`dirname $1`
-		realPath $RELDIR/$FILE
-	done
+function markAsSearched() {
+	if [ $1 ]; then
+		echo $1 >> $HISTORY_FILE
+		echo "`cat $SEARCH_FILE | grep -vFx $1`" > $SEARCH_FILE
+	fi
 }
 
-# Get the dependencies for the root file
-DEPENDENCIES=`findDependencies $SOURCE_FILE`
+function findDependencies() {
+	FOUND_LIST=./cache/found.list
+	if [ ! -e $1 ]; then
+		# markAsSearched $1
+		return 1
+	fi
 
-echo $DEPENDENCIES > dependencies.list
+	grep 'include(\|include_once(\|require(\|require_once(' $1 | grep -v '/\*' | grep -v 'require_once($\|require($\|include_once($\|include($' | sed -e "s/.*require('//g;s/.*require_once('//g;s/.*require(\"//g;s/.*require_once(\"//g;s/.*include('//g;s/.*include_once('//g;s/.*include(\"//g;s/.*include_once(\"//g;s/').*//g;s/\").*//g" | sort -u > $FOUND_LIST
 
-for DEPENDENCY in $DEPENDENCIES; do
-	findDependencies $DEPENDENCY >> dependencies.list
+	i=0
+
+	while read FILE; do
+		RELDIR=`dirname $1`
+		REALPATH=$(realPath $RELDIR/$FILE)
+    	i=$(expr $i + 1)
+
+    	# If file hasn't been searched, add it to search list
+    	if [ ! `grep -Fx $REALPATH $HISTORY_FILE` ]; then
+    		if [ -e $REALPATH ]; then
+    			echo $REALPATH >> $SEARCH_FILE
+    		fi
+    	fi
+	done < $FOUND_LIST
+
+	echo "$i dependencies found in $1"
+
+	# Mark this file as searched and remove it from the search file
+	markAsSearched $1
+}
+
+# Kick off the search by adding the specified file
+echo $1 > $SEARCH_FILE
+
+# While there are still things to check, look through them
+while sitesRemainToBeSearched; do
+	while read DEPENDENCY; do
+		findDependencies $DEPENDENCY
+	done < $SEARCH_FILE
 done
 
-# Remove any duplicates
-sort -u dependencies.list -o dependencies.list
-# rm -r cache/
+copyAllDependencies
